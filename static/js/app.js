38ee3b3f5d4586b42bedd1860188baccdaf7a490
James DeGurse <jamesdegurse@gmail.com>
2021-02-16 23:00:07 -0500

    Create _renderBoard()
    The _renderBoard() function runs through the gameBoard array and
    updates the square's textContent to reflect the array.

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,51 @@
+const DOMElements = (function () {
+    return {
+        s00: document.getElementById('s00'),
+        s01: document.getElementById('s00'),
+        s02: document.getElementById('s00'),
+        s10: document.getElementById('s00'),
+        s11: document.getElementById('s00'),
+        s12: document.getElementById('s00'),
+        s20: document.getElementById('s00'),
+        s21: document.getElementById('s00'),
+        s22: document.getElementById('s00')
+    }
+})();
+
+//////// GAME BOARD MODULE ////////
 const gameBoard = (function () {
     let array = {
         s00: '', s01: '', s02: '',
         s10: '', s11: '', s12: '',
         s20: '', s21: '', s22: '',
     }
+    const keys = Object.keys(array);
+
     function newBoard() {
-        for (const i in array) {
-            array[i] = ''
+        for (const key of keys) {
+            array[key] = '';
         }
     };
-    function markSquare(player) {
-        return;
+
+    function _renderBoard() {
+        for (const key of keys) {
+            if (array[key] === 'x') {
+                DOMElements[key].textContent = 'X';
+            }
+            else if (array[key] === 'o') {
+                DOMElements[key].textContent = 'O';
+            }
+            else {
+                DOMElements[key].textContent = '';
+            }
+        }
     };
+
+    function markSquare() {
+        alert('helloooooooo');
+        _renderBoard()
+    };
+
     return {
         array,
         newBoard,
@@ -19,8 +53,9 @@ const gameBoard = (function () {
     };
 })();
 
+//////// GAME CONTROLLER MODULE ////////
 const gameController = (function () {
-    const startGame = function () {
+    function startGame() {
         let game_over = false;
         // Main game loop
         while (!game_over) {
@@ -29,11 +64,18 @@ const gameController = (function () {
             // allow next player to play a move
         }
     };
+
+    function createEventListeners() {
+        DOMElements.s00.addEventListener('click', gameBoard.markSquare);
+    };
+
     return {
-        startGame
-    }
+        startGame,
+        createEventListeners
+    };
 })();
 
+//////// PLAYER FACTORY ////////
 function playerFactory(symbol, name) {
     return {
         symbol,
